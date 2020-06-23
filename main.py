640bf074e12e14bdce17cb59b311f577c368d60f
Shardul Srivastava <shardul.srivastava007@gmail.com>
2020-06-23 18:56:47 +0800

    Added Diagnostics Endpoint library

--- main.py
+++ main.py
@@ -5,11 +5,23 @@ from flask_cors import CORS
 
 from webapp import create_app
 
+from diagnostics_endpoint import Diagnostics
+
 env = os.environ.get("FLASK_ENV", "development")
 app = create_app("config.%sConfig" % env.capitalize())
 
 swagger = Swagger(app)
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+
+
+application_endpoints = [
+    {"name": "API Template", "endpoint": "/heartbeat"}
+]
+
+app = Flask(__name__)
+Diagnostics.render(app, application_endpoints)
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
